2cdb15f5806b89fa0b7eccc62e31190393f03924
Stig Brautaset <stig@circleci.com>
2022-10-07 11:37:52 +0100

    Basic tests for withdrawals

--- test/buckpal/account/domain/account_test.clj
+++ test/buckpal/account/domain/account_test.clj
@@ -16,3 +16,14 @@
     (is (= 1555 (a/calculate-balance (a/account 1 555 [(activity 2 1 999)
                                                        (activity 2 1 1)]))))))
 
+(deftest withdraw
+  (let [account (a/account 1 555 [(activity 2 1 999)
+                                  (activity 2 1 1)])]
+    (testing "it can withdraw money"
+      (let [after (a/withdraw account 555 99)]
+        (is (some? after))
+        (is (= 3 (count (:activity-window after))))
+        (is (= 1000 (a/calculate-balance after)))))
+
+    (testing "it can fail to withdraw money"
+      (is (nil? (a/withdraw account 1556 99))))))
